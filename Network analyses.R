21d0818dac31b8b06c975a3a90f9f270586c98c9
akarami1 <adrian.karami@hotmail.de>
2022-04-30 20:07:24 +0200

    1

--- Network analyses.R
+++ Network analyses.R
@@ -2,7 +2,24 @@
 library(bootnet)
 library(psychonetric)
 
-
+#Rename mismatch columns
+names(Evolution4)[names(Evolution4) == 'sleep'] <- 'M1'
+names(Evolution4)[names(Evolution4) == 'exercise'] <- 'M2'
+names(Evolution4)[names(Evolution4) == 'diet'] <- 'M3'
+names(Evolution4)[names(Evolution4) == 'unhealthy'] <- 'M4'
+names(Evolution4)[names(Evolution4) == 'medication'] <- 'M5'
+names(Evolution4)[names(Evolution4) == 'health'] <- 'M6'
+names(Evolution4)[names(Evolution4) == 'nature'] <- 'M7'
+names(Evolution4)[names(Evolution4) == 'joy'] <- 'M8'
+names(Evolution4)[names(Evolution4) == 'worrying'] <- 'M9'
+names(Evolution4)[names(Evolution4) == 'childhood'] <- 'M10'
+names(Evolution4)[names(Evolution4) == 'freedom'] <- 'M11'
+names(Evolution4)[names(Evolution4) == 'time'] <- 'M12'
+names(Evolution4)[names(Evolution4) == 'social'] <- 'M13'
+names(Evolution4)[names(Evolution4) == 'materialism'] <- 'M14'
+names(Evolution4)[names(Evolution4) == 'perfectionism'] <- 'M15'
+names(Evolution4)[names(Evolution4) == 'individualism'] <- 'M16'
+names(Evolution4)[names(Evolution4) == 'socialmedia'] <- 'M17'
 
 #Estimate a network with correction
 evolution_network <- estimateNetwork(Evolution4,default = "EBICglasso",
@@ -15,16 +32,19 @@ evolution_network$graph
 
 #Plot network
 plot(evolution_network,layout = "spring",
-     nodeNames = Names,
-     legend.cex = 0.3,
      groups = Grouping,
      theme = "colorblind",
-     vsize = 4,
+     vsize = 5,
      GLratio = 1.5,
      filetype = "pdf",
-     filename = "first network")
+     filename = "names")
 
-
+# Plot without saving 
+plot(evolution_network,layout = "spring",
+     groups = Grouping,
+     theme = "colorblind",
+     vsize = 8,
+     GLratio = 1.5)
 
 # Names
 Names <- c("Little interest or pleasure in doing things",
@@ -62,7 +82,10 @@ Grouping[10:26] <- "Mismatch Factors"
 
 #Centrality measures
 library(qgraph)
-centralityPlot(evolution_network)
+centralityPlot(evolution_network,include = 
+                 c("Strength","Betweenness","Closeness",
+                   orderBy = "default"))
+
 
 ##Test for stability of centrality indices
 CentralStability <- bootnet(evolution_network, nBoots = 1000, type = "case",statistics = c("strength", "betweenness", "closeness"))
@@ -71,13 +94,28 @@ CentralStability <- bootnet(evolution_network, nBoots = 1000, type = "case",stat
 corStability(CentralStability)
 
 # Plot stability of centrality indices 
-plot(CentralStability)
+plot(CentralStability, statistics = c("strength", "betweenness", "closeness"))
+
+# Test fo differences in centrality 
+plot(CentralStability,
+     plot = "difference",
+     onlyNonZero = TRUE,
+     order = "sample"
+)
 
 ##Test for edege weight accuracy 
-EdgeWgt <- bootnet(evolution_network, nBoots = 2500)
+EdgeWgt <- bootnet(evolution_network, nBoots = 500)
 
 #Summary table of results of edge weight accuracy 
-summary(EdgeWgt)
+f <- summary(EdgeWgt) 
+View(f)
 
 #Plot edge weight accuracy
-plot(EdgeWgt, labels = TRUE, order = "sample")
+plot(EdgeWgt, labels = FALSE, order = "sample")
+
+#Plot edge differences
+plot(EdgeWgt,
+     plot = "difference",
+     onlyNonZero = TRUE,
+     order = "sample"
+     )
